e56c1a2547032baa7c59e6233cfb15bc80004c0a
vlubosh <Lubo2k20*>
2022-02-08 14:54:03 +0100

    added click function on first letter

--- app.js
+++ app.js
@@ -11,6 +11,21 @@ function playsound(e) {
   audio.play();
 }
 
+document.querySelector(".key650").onclick = function () {
+  myFunction();
+};
+
+function myFunction() {
+    let key = document.querySelector(`.key650`);
+    let audio = document.querySelector(`.key651`);
+
+    key.classList.add("playing");
+
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+}
+
 function removeTransition(e) {
   this.classList.remove("playing");
 }
