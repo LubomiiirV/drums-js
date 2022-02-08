8a8116cdfaf258fa4685688afca6e3f2acfbf52f
vlubosh <Lubo2k20*>
2022-02-08 14:21:11 +0100

    refactoring

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-window.addEventListener("keydown", function (e) {
-  let element = document.querySelector(`div[data-key="${e.keyCode}"]`);
+window.addEventListener("keydown", playsound);
+
+function playsound(e) {
+  let key = document.querySelector(`div[data-key="${e.keyCode}"]`);
   let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
 
-  element.classList.add("playing");
+  key.classList.add("playing");
 
   if (!audio) return;
   audio.currentTime = 0;
   audio.play();
-});
+}
 
 function removeTransition(e) {
-    this.classList.remove("playing");  
-};
+  this.classList.remove("playing");
+}
 
 const keys = document.querySelectorAll(".key");
 keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+
+
