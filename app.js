97375fda8bf97d39d23637be3318eab3ff66f10c
vlubosh <Lubo2k20*>
2022-02-18 14:06:59 +0100

    small fixes, let to const

--- app.js
+++ app.js
@@ -18,82 +18,81 @@ keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
 
 //playing with keyborad
 function playsound(e) {
-  let key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-  let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
 
   addSound(key, audio);
 }
 
 //playing on mouse click
-
 //letter A
 document.querySelector(".A").onclick = function playA() {
-  let key = document.querySelector(".A");
-  let audio = document.querySelector(".audioA");
+  const key = document.querySelector(".A");
+  const audio = document.querySelector(".audioA");
 
   addSound(key, audio);
 };
 
 //letter S
 document.querySelector(".S").onclick = function playS() {
-  let key = document.querySelector(".S");
-  let audio = document.querySelector(".audioS");
+  const key = document.querySelector(".S");
+  const audio = document.querySelector(".audioS");
   addSound(key, audio);
   
 };
 
 //letter D
 document.querySelector(".D").onclick = function playS() {
-  let key = document.querySelector(".D");
-  let audio = document.querySelector(".audioD");
+  const key = document.querySelector(".D");
+  const audio = document.querySelector(".audioD");
 
   addSound(key, audio);
 };
 
 //letter F
 document.querySelector(".F").onclick = function playS() {
-  let key = document.querySelector(".F");
-  let audio = document.querySelector(".audioF");
+  const key = document.querySelector(".F");
+  const audio = document.querySelector(".audioF");
 
   addSound(key, audio);
 };
 
 //letter G
 document.querySelector(".G").onclick = function playS() {
-  let key = document.querySelector(".G");
-  let audio = document.querySelector(".audioG");
+  const key = document.querySelector(".G");
+  const audio = document.querySelector(".audioG");
 
   addSound(key, audio);
 };
 
 //letter H
 document.querySelector(".H").onclick = function playS() {
-  let key = document.querySelector(".H");
-  let audio = document.querySelector(".audioH");
+  const key = document.querySelector(".H");
+  const audio = document.querySelector(".audioH");
 
   addSound(key, audio);
 };
 
 //letter J
 document.querySelector(".J").onclick = function playS() {
-  let key = document.querySelector(".J");
-  let audio = document.querySelector(".audioJ");
+  const key = document.querySelector(".J");
+  const audio = document.querySelector(".audioJ");
 
   addSound(key, audio);
 };
 
 //letter K
 document.querySelector(".K").onclick = function playS() {
-  let key = document.querySelector(".K");
-  let audio = document.querySelector(".audioK");
+  const key = document.querySelector(".K");
+  const audio = document.querySelector(".audioK");
 
   addSound(key, audio);
 };
 
 //letter L
 document.querySelector(".L").onclick = function playS() {
-  let key = document.querySelector(".L");
-  let audio = document.querySelector(".audioL");
+  const key = document.querySelector(".L");
+  const audio = document.querySelector(".audioL");
 
   addSound(key, audio);
 };
